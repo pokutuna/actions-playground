65f2c83a1f8cdf346b64339167fe3fd8eb890861
pokutuna <mail@pokutuna.com>
2021-11-25 05:51:22 +0900

    try without scope

---
+++ examples/access-spreadsheet/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
+)
+
+func main() {
+	ctx := context.Background()
+	client, err := sheets.NewService(ctx, option.WithQuotaProject("pokutuna-playground"))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+	}
+
+	spreadsheetId := "1Vrmv8_h2npEkqVLZOjUlS-tKvy0vQW-hiY03_xWJuxo"
+	resp, err := client.Spreadsheets.Values.Get(spreadsheetId, "シート1!A1:B2").Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from sheet: %v", err)
+	}
+	for _, row := range resp.Values {
+		for _, col := range row {
+			fmt.Printf("%s,", col)
+		}
+		fmt.Printf("\n")
+	}
+}
